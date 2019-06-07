de42da8dcd06c4a4b4bd6739ac03aef7193462bb
Samu Suvanto <samu.suvanto@gmail.com>
2019-06-07 12:33:34 +0300

    fix click coordinate source. add offset. add helper funcs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,34 @@ import { Component, OnInit } from '@angular/core'
 })
 export class AppComponent implements OnInit {
   ctxt: CanvasRenderingContext2D
-  lw = 30
+  cw = 30 //cell width/height
+  cell_offset_x = -9
+  cell_offset_y = -9
 
   canvClick(me: MouseEvent) {
-    const x_cell = Math.floor(me.x/this.lw)
-    const y_cell = Math.floor(me.y/this.lw)
-    this.cellClick(x_cell, y_cell)
+    console.log('Point:', me.offsetX, me.offsetY)
+    const {x, y} = this.canvToCell(me.offsetX, me.offsetY)
+    this.cellClick(x, y)
   }
 
   cellClick(x, y){
-    console.log('Clicked at', x, y)
-    this.ctxt.fillRect(x * this.lw + 5, y * this.lw + 5, this.lw - 10, this.lw -10)
+    console.log('Cell:', x, y)
+    const {x: px, y: py} = this.cellToCanv(x, y)
+    this.ctxt.fillRect(px + 5, py + 5, this.cw - 10, this.cw -10)
+  }
+
+  //Returns the cell based on pixel coordinates x,y
+  canvToCell(x, y) {
+    const x_cell = Math.floor(x/this.cw) + this.cell_offset_x
+    const y_cell = Math.floor(y/this.cw) + this.cell_offset_y
+    return {x: x_cell ,y: y_cell}
+  }
+
+  //Returns the top left pixel coordinates of the cell at x,y
+  cellToCanv(x, y) {
+    const x_pix = (x - this.cell_offset_x) * this.cw
+    const y_pix = (y - this.cell_offset_y) * this.cw
+    return {x: x_pix, y: y_pix}
   }
 
   initCanvas() {
@@ -28,12 +45,12 @@ export class AppComponent implements OnInit {
 
     this.ctxt.strokeStyle = "black"
 
-    for(let x=this.lw;x<=w;x+=this.lw){
+    for(let x=this.cw;x<=w;x+=this.cw){
       this.ctxt.moveTo(0.5 + x, 0)
       this.ctxt.lineTo(0.5 + x, h)
     }
 
-    for(let y=this.lw;y<h;y+=this.lw){
+    for(let y=this.cw;y<h;y+=this.cw){
       this.ctxt.moveTo(0, 0.5 + y)
       this.ctxt.lineTo(w, 0.5 + y)
     }
