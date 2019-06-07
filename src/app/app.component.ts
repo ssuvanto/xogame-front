cefa9109dba899d27542775d08c5a9ee6badfa8b
Samu Suvanto <samu.suvanto@gmail.com>
2019-06-07 11:36:31 +0300

    click to mark grid nodes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,41 @@ import { Component, OnInit } from '@angular/core'
 })
 export class AppComponent implements OnInit {
   ctxt: CanvasRenderingContext2D
+  lw = 30
 
+  canvClick(me: MouseEvent) {
+    const x_cell = Math.floor(me.x/this.lw)
+    const y_cell = Math.floor(me.y/this.lw)
+    this.cellClick(x_cell, y_cell)
+  }
 
-  ngOnInit() {
+  cellClick(x, y){
+    console.log('Clicked at', x, y)
+    this.ctxt.fillRect(x * this.lw + 5, y * this.lw + 5, this.lw - 10, this.lw -10)
+  }
+
+  initCanvas() {
     const canvas = <HTMLCanvasElement>document.getElementById('canvas')
     this.ctxt = canvas.getContext('2d')
     const w = canvas.width
     const h = canvas.height
-    const lw = 30
 
     this.ctxt.strokeStyle = "black"
 
-    for(let x=lw;x<=w;x+=lw){
+    for(let x=this.lw;x<=w;x+=this.lw){
       this.ctxt.moveTo(0.5 + x, 0)
       this.ctxt.lineTo(0.5 + x, h)
     }
 
-    for(let y=lw;y<h;y+=lw){
+    for(let y=this.lw;y<h;y+=this.lw){
       this.ctxt.moveTo(0, 0.5 + y)
       this.ctxt.lineTo(w, 0.5 + y)
     }
 
     this.ctxt.stroke()
+  }
+
+  ngOnInit() {
+    this.initCanvas()
   }
 }
