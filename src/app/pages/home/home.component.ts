88f375be9a1066aa2e5fc5861929634f671aa360
Samu Suvanto <samu.suvanto@gmail.com>
2019-06-12 11:10:54 +0300

    simple echo socket test interface

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
 import { UserService } from 'src/app/user.service'
 
 declare var SockJS
@@ -8,23 +8,37 @@ declare var SockJS
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   constructor(private user: UserService) {}
+  sockopen = false
   sock = null
+  messages = []
+
+  sendMsg(msg) {
+    if(msg){
+      this.sock.send(msg)
+    }
+  }
 
   ngOnInit() {
     console.log('Opening socket...')
     this.sock = new SockJS('http://localhost:4444/sock')
     this.sock.onopen = () => {
       console.log('Socket opened')
-      this.sock.send('hello')
+      this.sockopen = true
     }
     this.sock.onmessage = (m) => {
       console.log('Received',m.data)
-      this.sock.close()
+      this.messages.push(m.data)
     }
     this.sock.onclose = () => {
       console.log('Socket closed')
+    }
+  }
+
+  ngOnDestroy() {
+    if(this.sockopen){
+      this.sock.close()
     }
   }
 }
