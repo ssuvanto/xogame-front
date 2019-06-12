77aaa299cd76ddef70175076156993060defe1c6
Samu Suvanto <samu.suvanto@gmail.com>
2019-06-12 10:50:56 +0300

    implement sockjs, through script tag instead of package because package doesn't work

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,30 @@
-import { Component, Input } from '@angular/core';
-import { UserService } from 'src/app/user.service';
+import { Component, OnInit } from '@angular/core'
+import { UserService } from 'src/app/user.service'
+
+declare var SockJS
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  constructor(private user: UserService) {
-    
+export class HomeComponent implements OnInit {
+  constructor(private user: UserService) {}
+  sock = null
+
+  ngOnInit() {
+    console.log('Opening socket...')
+    this.sock = new SockJS('http://localhost:4444/sock')
+    this.sock.onopen = () => {
+      console.log('Socket opened')
+      this.sock.send('hello')
+    }
+    this.sock.onmessage = (m) => {
+      console.log('Received',m.data)
+      this.sock.close()
+    }
+    this.sock.onclose = () => {
+      console.log('Socket closed')
+    }
   }
 }
